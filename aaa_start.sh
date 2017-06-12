e82f0e97e3f8b853c1fd00358a02cc927ab9a55e
Ashish Mishra <ashishkumarmishra235@gmail.com>
2017-06-12 11:00:01 +0530

    Create aaa_start.sh

---
+++ aaa_start.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -euo pipefail
+IFS=$'\n\t'
+
+#/ Usage:
+#/ Description:
+#/ Examples:
+#/ Options:
+#/   --help: Display this help message
+AAA_HOME="/home/alepo/alepoaaa"
+ORACLE_HOME="/u01/app/oracle/product/11.2.0/client"
+
+if [[ -z $AAA_HOME ]]; then
+	echo "$AAA_HOME is not set"
+	 exit 1
+fi
+
+if [ ! `su - alepo -c "$AAA_HOME/alepoaaa status| grep 'running' " ` ]; then
+	su alepo -c "$AAA_HOME/alepoaaa start"
+	echo "AAA starting"
+	
+fi
