4cdc29cf988c8d55066474ab5154818ffcc053c9
Ashish Mishra <ashishkumarmishra235@gmail.com>
2017-07-24 11:21:26 +0530

    Update aaa_heal.sh
    Updated

--- aaa_heal.sh
+++ aaa_heal.sh
@@ -1,1 +1,42 @@
-echo "Healed" > /tmp/heal.log 
+#!/usr/bin/env bash
+set -euo pipefail
+IFS=$'\n\t'
+
+#/ Usage:
+#/ Description:
+#/ Examples:
+#/ Options:
+#/   --help: Display this help message
+AAA_HOME="/home/alepo/alepoaaa"
+ORACLE_HOME="/u01/app/oracle/product/11.2.0/client"
+
+if [[ -z $AAA_HOME ]]; then
+	echo "$AAA_HOME is not set"
+	 exit 1
+fi
+
+cause=$1
+case "$cause" in
+
+("serviceDown") 
+	echo "The AAA is down, let's try to restart it..."
+	#check if pid file is present, if yes, then delete the pid file.
+	if	[[  -f  $AAA_HOME/alepoaaaserver.pid ]]; then
+			su - alepo -c "rm -fr $AAA_HOME/alepoaaaserver.pid"
+	fi
+	# start AAA services.
+	su alepo -c "$AAA_HOME/alepoaaa start"
+	# check if AAA services successfully started
+	if [ ! `su - alepo -c "$AAA_HOME/alepoaaa status| grep 'running' " ` ]; then
+		echo "ERROR: the AAA is not started"
+	    exit 1
+	
+	fi
+	
+	echo "The AAA is running again!"
+	;;
+*) echo "The cause $cause is unknown"
+	exit 2
+	;;
+esac
+
